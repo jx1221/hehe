a7480925bac3db1feb9d85d11a8c8dfb8b6f5742
jx1221 <792250977@qq.com>
2017-10-12 17:16:01 +0800

    downandmove

--- drag.js
+++ drag.js
@@ -3,4 +3,10 @@ box.addEventListener('mousedown','down');
 function down(event){
 	var disX = event.clientX - this.offsetLeft;
 	var disY = event.clientY - this.offsetTop;
+	document.addEventListener('mousemove','move');
+	function move(event){
+		box.style.left = event.clientX -disX + 'px';
+		box.style.top = event.clientY -disY + 'px';
+	}
+	
 }
