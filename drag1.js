52fde4607a529aa1361b54d0505e31d0edab1b31
jx1221 <792250977@qq.com>
2017-10-12 17:56:01 +0800

    up1

--- drag1.js
+++ drag1.js
@@ -1,1 +1,9 @@
 var box = document.querySelector('.box');
+
+
+	document.addEventListener('mouseup',up);
+	function up(){
+		document.removeEventListener('mousemove',move);
+		document.removeEventListener('mouseup',up);
+	}
+
