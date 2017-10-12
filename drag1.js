82a6845a0c2fb03823305f6d3c55133df51f4962
jx1221 <792250977@qq.com>
2017-10-12 17:42:05 +0800

    down1

--- drag1.js
+++ drag1.js
@@ -1,1 +1,9 @@
 var box = document.querySelector('.box');
+box.addEventListener('mousedown',down);
+function down(event){
+
+	var disX = event.clientX - this.offsetLeft;
+	var disY = event.clientY - this.offsetTop;
+	
+
+}
