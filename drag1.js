b4b02aa5333c944b4787dedf7b634c71faa0777b
jx1221 <792250977@qq.com>
2017-10-12 17:53:54 +0800

    move1

--- drag1.js
+++ drag1.js
@@ -1,1 +1,9 @@
 var box = document.querySelector('.box');
+
+	document.addEventListener('mousemove',move);
+	function move(event){
+		box.style.left = event.clientX -disX + 'px';
+		box.style.top = event.clientY -disY + 'px';
+	}
+
+	
